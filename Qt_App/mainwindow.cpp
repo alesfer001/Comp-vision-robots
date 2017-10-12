e6462ea9d72d7d08430e2515a86830f6ec7ffaf6
Lesfer Ayoub <lesferayoub@gmail.com>
2017-10-12 23:08:32 +0200

    TP1

--- Qt_App/mainwindow.cpp
+++ Qt_App/mainwindow.cpp
@@ -1,32 +1,55 @@
 #include "mainwindow.h"
-#include <QMenu>
-#include <QMenuBar>
-#include <QAction>
-#include <QMessageBox>
-#include <QApplication>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
     QAction *quit_action = new QAction("&Quit", this);
-    QAction *buy = new QAction("&Buy Product", this);
+    QAction *open_action = new QAction("&Open", this);
 
-    QMenu *file;
+    QMenu *file = new QMenu("My Menu");
     file = menuBar()->addMenu("&File");
+    file->addAction(open_action);
     file->addAction(quit_action);
 
-    QMenu *apropos;
-    apropos = menuBar()->addMenu("&A propos");
-    apropos->addAction(buy);
-
-    QMessageBox msgBox;
-    msgBox.setText("Hello World!");
-    msgBox.exec();
-
-    //connect(quit_action, &QAction::triggered, this, QApplication::quit);
-    connect(quit_action, SIGNAL(triggered()), qApp, SLOT(quit()));
+    connect(quit_action, SIGNAL(triggered(bool)),
+            this, SLOT(my_quit()));
+    connect(open_action, SIGNAL(triggered(bool)),
+            this, SLOT(openFile()));
 }
 
 MainWindow::~MainWindow()
 {
+}
+
+void MainWindow::my_quit(){
+    QMessageBox *mybox = new QMessageBox();
+    mybox->setText("App exited succesfully!");
+    mybox->exec();
+    QApplication::quit();
+}
+
+void MainWindow::openFile(){
+    QString file_name = QFileDialog::getOpenFileName(this, tr("Open File"), "/home/alesfer", tr("Images (*.png *.jpg *.gif)"));
+
+    QMessageBox *mybox = new QMessageBox();
+
+    QImageReader *my_img_reader = new QImageReader(file_name);
+    QImage my_img = my_img_reader->read();
+
+    QPixmap my_pixmap;
+    my_pixmap = QPixmap::fromImage(my_img);
+    my_pixmap = my_pixmap.scaled(1024,576);
+
+
+    MainWindow::my_label.setPixmap(my_pixmap);
+    MainWindow::my_label.adjustSize();
+    MainWindow::my_label.setFixedSize(1024,576);
+
+    if(file_name != ""){
+        MainWindow::my_label.show();
+    }
+    else{
+        mybox->setText("You have canceled!");
+        mybox->exec();
+    }
 }
--- Qt_App/mainwindow.h
+++ Qt_App/mainwindow.h
@@ -2,14 +2,31 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QMenu>
+#include <QMenuBar>
+#include <QAction>
+#include <QMessageBox>
+#include <QApplication>
+#include <QFileDialog>
+#include <QImageReader>
+#include <QImage>
+#include <QPixmap>
+#include <QLabel>
+#include <QResizeEvent>
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
+    QLabel my_label;
     MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    void resizeEvent(QResizeEvent *);
+
+public slots:
+    void my_quit();
+    void openFile();
 };
 
 #endif // MAINWINDOW_H
